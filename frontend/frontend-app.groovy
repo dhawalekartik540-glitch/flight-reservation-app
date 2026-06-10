941671bacfbe2570c6517e9ed2231f2687758671
dhawalekartik540-glitch <dhawalekartik540@gmail.com>
2026-06-10 15:00:46 +0530

    new pipeline is added

--- frontend/frontend-app.groovy
+++ frontend/frontend-app.groovy
@@ -6,20 +6,25 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/dhawalekartik540-glitch/flight-reservation-app.git'    
             }
         }
-        stage('Code-Build'){
-            steps{
-                sh '''
-                    cd frontend
-                    npm install
-                    npm run build
-                '''
-            }
-        }
+        // stage('Code-Build'){
+        //     steps{
+        //         sh '''
+        //             cd frontend
+        //             npm install
+        //             npm run build
+        //         '''
+        //     }
+        // }
         stage('Deploy'){
             steps{
                 sh '''
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-cred']]) {
+                accessKeyId = env.AWS_ACCESS_KEY_ID
+                secretAccessKey = env.AWS_SECRET_ACCESS_KEY
+                }
                 cd frontend
-                aws s3 sync dist/ s3://cbz-frontend-project-bux / 
+                aws s3 ls
+                
                 '''  
             }
         }
